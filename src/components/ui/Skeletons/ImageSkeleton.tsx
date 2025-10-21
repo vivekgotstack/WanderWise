bcf9505c43fa7bc05e633dc1e73e8e3489daff9c
Vivek <vivekni1224@gmail.com>
2025-10-21 18:04:10 +0530

    Refactor ImageSkeleton component for theme support

--- src/components/ui/Skeletons/ImageSkeleton.tsx
+++ src/components/ui/Skeletons/ImageSkeleton.tsx
@@ -5,23 +5,20 @@ export default function SkeletonCard() {
     const { theme } = useTheme();
 
     const skeletonColor = theme === "light" ? "bg-gray-300" : "bg-gray-700";
-    const bgGradient =
-        theme === "light"
-            ? "bg-gradient-to-r from-white to-gray-200"
-            : "bg-gradient-to-r from-gray-900 to-gray-700";
 
     return (
-        <div
-            className={`flex flex-col md:flex-row justify-center items-center w-full pt-32 md:pt-20 ${bgGradient} gap-8 px-4`}
-        >
-            <Skeleton
-                className={`${skeletonColor}
-                h-48 w-3/4  
-                sm:h-64 sm:w-3/4   
-                md:h-80 md:w-1/2  
-                lg:h-96 lg:w-1/2  
+        <Skeleton
+            className={`
+            ${theme === "light"
+                    ? "bg-gradient-to-r from-white to-gray-200"
+                    : "bg-gradient-to-r from-gray-900 to-gray-700"
+                }
+             ${skeletonColor}
+                h-38 w-5/6  
+                sm:h-70 sm:w-full   
+                md:h-80 md:w-full  
+                lg:h-96 lg:w-full  
                 rounded-xl`}
-            />
-        </div>
+        />
     );
 }
